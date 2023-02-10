9841630cbd3ee49209b266197d0992efa934be94
osj <os133517@gmail.com>
2023-02-11 00:49:38 +0900

    상품 등록 구현

--- src/modules/RecipeModule.js
+++ src/modules/RecipeModule.js
@@ -10,6 +10,7 @@ export const GET_RECIPES_RECOMMEND = 'recipe/GET_RECIPES_RECOMMEND';
 export const GET_RECIPE = 'recipe/GET_RECIPE';
 export const PUT_RECIPE_RECOMMEND = 'recipe/PUT_RECIPE_RECOMMEND';
 export const DELETE_RECIPE = 'recipe/DELETE_RECIPE';
+export const POST_RECIPE = 'recipe/POST_RECIPE';
 // eslint-disable-next-line
 const actions = createActions({
     [GET_RECIPES_TOP] : () => {},
@@ -17,7 +18,8 @@ const actions = createActions({
     [GET_RECIPES_RECOMMEND] : () => {},
     [GET_RECIPE] : () => {},
     [PUT_RECIPE_RECOMMEND] : () => {},
-    [DELETE_RECIPE] : () => {}
+    [DELETE_RECIPE] : () => {},
+    [POST_RECIPE] : () => {}
 });
 
 // 리듀서
@@ -27,7 +29,8 @@ const recipeReducer = handleActions({
     [GET_RECIPES_RECOMMEND] : (state, {payload}) => payload,
     [GET_RECIPE] : (state, {payload}) => payload,
     [PUT_RECIPE_RECOMMEND] : (state, {payload}) => payload,
-    [DELETE_RECIPE] : (state, {payload}) => payload
+    [DELETE_RECIPE] : (state, {payload}) => payload,
+    [POST_RECIPE] : (state, {payload}) => payload
 }, initialState);
 
 export default recipeReducer;
